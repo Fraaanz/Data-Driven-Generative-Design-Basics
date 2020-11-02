35e8fb558332c7b2ce847995c3f647f21485a0dd
Franz Mattuschka <franz.mattuschka@experienceone.com>
2020-11-02 16:47:37 +0100

    light snippes

---
+++ 3d-parts/js-snippets/threejs-lights.js
@@ -0,0 +1,14 @@
+// HemisphereLight(skyColor: any, groundColor: any, intensity: any)
+var light;
+light = new THREE.HemisphereLight(0xFFFFFF, 0x0000FF, 0.5,);
+scene.add(light);
+
+// PointLight(color: any, intensity: any, distance: any, decay: any)
+var light = new THREE.PointLight(0xFFFFFF, 20, 300,);
+light.position.set(0, 100, -210);
+scene.add(light);
+
+// SpotLight(color: any, intensity: any, distance: any, angle: any, penumbra: any, decay: any)
+var spotLight = new THREE.SpotLight(0xFFFFFF, 2, 100, 0.3, 0.5, 2,);
+spotLight.position.set(0, 3, 10);
+scene.add(spotLight);
